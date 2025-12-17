974d2af9b9ab0c086f308026bbdd0c4d901a7134
wsgtl <2568271372@qq.com>
2025-12-17 10:11:50 +0800

    feat:修改

--- native/engine/android/app/src/com/cocos/game/AppsFlyer.java
+++ native/engine/android/app/src/com/cocos/game/AppsFlyer.java
@@ -28,7 +28,7 @@ public class AppsFlyer
     private String Tag = "afEvent";
     private Activity mApplication = null;
     /**af的key*/
-    private String AfDevKey = "bczkaJCR5tXG4VETeTA2Ea";
+    private String AfDevKey = "sxVJt3RmoPmfXmJgTQbNc9";
     /**af初始化*/
     public void init(Activity application){
         mApplication = application;
