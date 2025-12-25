841c77d30fdd39e803b9fa2a321bf85d56255277
wsgtl <2568271372@qq.com>
2025-12-25 11:35:05 +0800

    feat：埋点优化ab测试

--- native/engine/android/app/src/com/cocos/game/AppsFlyer.java
+++ native/engine/android/app/src/com/cocos/game/AppsFlyer.java
@@ -34,6 +34,8 @@ public class AppsFlyer
     private Activity mApplication = null;
     /**af的key*/
     private String AfDevKey = "QGnEbf7ihrNdXHjdyiPHoU";
+    private String AbTestStr = "A";
+>>>>>>> b469257 (feat：埋点优化ab测试)
     /**af初始化*/
     public void init(Activity application){
         mApplication = application;
@@ -55,6 +57,11 @@ public class AppsFlyer
 
         JsbBridgeWrapper jbw = JsbBridgeWrapper.getInstance();
         jbw.addScriptEventListener("sendEvent",this::sendEvent);
+        jbw.addScriptEventListener("setAb",this::setAb);
+    }
+    /**设置AB*/
+    private void setAb(String s){
+        AbTestStr = s;
     }
     /**埋点上报*/
     public void sendEvent(String data) {
@@ -157,6 +164,7 @@ public class AppsFlyer
         data.put("placement", placement);
         data.put("ad_type", adType);
         this.baseSendEvent("ad_shown", data);
+        this.baseSendEvent(adType+"_show_"+this.AbTestStr,null);//广告设置abtest
     }
 
     /** 广告点击埋点 */
@@ -166,6 +174,7 @@ public class AppsFlyer
         data.put("placement", placement);
         data.put("ad_type", adType);
         this.baseSendEvent("ad_clicked", data);
+        this.baseSendEvent(adType+"_click_"+this.AbTestStr,null);//广告设置abtest
     }
 
     // 在AppsFlyer类里新增方法，保存当前placement，给上报用
